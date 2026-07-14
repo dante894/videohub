b0e37e340d501b4b58e9c7d038fe48f0169dade9
DanteTrb <trabalon.dante@gmail.com>
2026-07-14 14:35:51 -0300

    corregir main

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from threading import Thread
-
 from flask import Flask
 
 from app.config import HOST, PORT, FLASK_SECRET_KEY
 from app.socket_manager import socketio
 from app.web.routes import web
-from app.telegram.bot import run as run_bot
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = FLASK_SECRET_KEY
@@ -16,13 +14,7 @@ app.register_blueprint(web)
 
 if __name__ == "__main__":
 
-    # Iniciar el bot de Telegram en segundo plano
-    telegram_thread = Thread(
-        target=run_bot,
-        daemon=True,
-        name="TelegramBot"
-    )
-    telegram_thread.start()
+    
 
     # Iniciar Flask + SocketIO
     #
